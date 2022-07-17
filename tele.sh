c2a3c52f7d17dbd18756bb1b1e772a6ff52f83f4
kajilowo <109421926+kajilowo@users.noreply.github.com>
2022-07-17 08:05:41 +0700

    Add files via upload

---
+++ tele.sh
@@ -0,0 +1,56 @@
+clear
+blue='\033[34;1m' 
+green='\033[32;1m'
+ purple='\033[35;1m' 
+cyan='\033[36;1m'
+ red='\033[31;1m'
+ white='\033[37;1m' 
+yellow='\033[33;1m' 
+sleep 1
+echo
+toilet -f big -F gay by tukijo
+echo $green"selamat datang"
+echo
+sleep 3
+echo $yelow"silahkan pilih toolnya :"
+echo
+echo  $cyan"1.) pilih config openvpn"
+echo
+echo $cyan"2.) pilih config m3"
+echo
+echo $blue"0.) keluar aja bro !!!"
+echo
+echo $white
+read -p "pilih mana : " bro
+
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay by tukijo
+echo $green"config by tukijo"
+sleep 4
+echo $blue"https://sfile.mobi/jG66SWzS1y7"
+fi
+echo
+echo
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay by tukijo
+echo $red"config by tukijo"
+sleep 4
+echo $blue"https://sfile.mobi/OEKo4pFpo47"
+fi
+echo
+echo
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+clear
+echo $blue
+figlet " tukijo"
+echo $yelow "tobe be contued"
+sleep 3
+exit4
+fi
+
